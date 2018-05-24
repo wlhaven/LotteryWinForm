716bbb4a573ddc1aef470fadb575e9f5edc5da1a
Wally Haven <wlhaven@gmail.com>
2018-05-24 12:30:43 -0700

    Add project files.

---
+++ Lottery/mainFrm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Lottery
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.generateNumbersButton = new System.Windows.Forms.Button();
+            this.printNumbersButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many sets of numbers do you wish to play?";
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.Black;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(92, 87);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(645, 270);
+            this.listView1.TabIndex = 7;
+            this.listView1.TabStop = false;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Number 1";
+            this.columnHeader1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Number 2";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Number 3";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Number 4";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 80;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Number 5";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Number 6";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 80;
+            // 
+            // generateNumbersButton
+            // 
+            this.generateNumbersButton.Location = new System.Drawing.Point(140, 385);
+            this.generateNumbersButton.Name = "generateNumbersButton";
+            this.generateNumbersButton.Size = new System.Drawing.Size(144, 43);
+            this.generateNumbersButton.TabIndex = 2;
+            this.generateNumbersButton.Text = "Generate numbers";
+            this.generateNumbersButton.UseVisualStyleBackColor = true;
+            this.generateNumbersButton.Click += new System.EventHandler(this.generateNumberButton_Click);
+            // 
+            // printNumbersButton
+            // 
+            this.printNumbersButton.Location = new System.Drawing.Point(334, 385);
+            this.printNumbersButton.Name = "printNumbersButton";
+            this.printNumbersButton.Size = new System.Drawing.Size(113, 43);
+            this.printNumbersButton.TabIndex = 3;
+            this.printNumbersButton.Text = "Print ";
+            this.printNumbersButton.UseVisualStyleBackColor = true;
+            this.printNumbersButton.Click += new System.EventHandler(this.printNumbersButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(505, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(497, 385);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(102, 43);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.printNumbersButton);
+            this.Controls.Add(this.generateNumbersButton);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lottery Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button generateNumbersButton;
+        private System.Windows.Forms.Button printNumbersButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ Lottery/mainFrm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery
+{
+    public partial class mainFrm : Form
+    {
+        const int SIZE = 6;
+        const int MEGABUCKS = 48;
+        int [] lotto_ticket = new int[SIZE];
+        ListViewItem itm;
+
+        public mainFrm()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void generateNumberButton_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            try
+            {
+                var num_boards = int.Parse(textBox1.Text);
+                for (int index = 0; index < num_boards; index++)
+                {
+                    for (int i = 0; i < SIZE; i++)
+                    {
+                        int randomNumber = rand.Next(MEGABUCKS + 1);
+                        while(randomNumber == 0)
+                        {
+                            randomNumber = rand.Next(MEGABUCKS + 1);
+                        }
+                        lotto_ticket[i] = randomNumber;
+                        var tmp_num = lotto_ticket[i];
+                        var flag = Search(lotto_ticket, tmp_num, i);
+                        while (flag)
+                        {
+                            tmp_num = lotto_ticket[i];
+                            flag = Search(lotto_ticket, tmp_num, i);
+                            if(flag)
+                            {
+                                randomNumber = rand.Next(MEGABUCKS + 1);
+                                while (randomNumber == 0)
+                                {
+                                    randomNumber = rand.Next(MEGABUCKS + 1);
+                                }
+                                lotto_ticket[i] = randomNumber;
+
+                            }
+                            
+                        }
+                    }
+                    Array.Sort(lotto_ticket);
+                    string[] result = lotto_ticket.Select(x => x.ToString()).ToArray();
+                    itm = new ListViewItem(result);
+                    listView1.Items.Add(itm);
+                }
+                       
+            }
+            catch
+            {
+                MessageBox.Show("Input must be a number", "Error!");
+            }
+            
+        }
+        bool Search(int[] lotto_ticket, int num, int size)
+        {
+            int count = 0;
+            while (count < size)
+            {
+                if (num == lotto_ticket[count])
+                {
+                    return (true);
+                }                    
+                count++;
+            }
+            return (false);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e) => listView1.Items.Clear();
+
+        private void printNumbersButton_Click(object sender, EventArgs e)
+        {
+            if (!listView1.Items.Count.Equals(0))
+            {
+                //call method to prompt user for printing.
+                MessageBox.Show("Please make your printer ready", "Warning!");
+                CaptureScreen();
+            }
+            else
+            {
+                MessageBox.Show("Nothing to print!", "Error!");
+                textBox1.Focus();
+                return;
+            }
+        }
+
+        private void CaptureScreen()
+        {
+            //create a dialog object to select printer 
+            //and print the document to a file or a printer.
+            try
+            {
+                PrintDialog printDialog1 = new PrintDialog();
+                printDialog1.Document = printDocument1;
+                DialogResult result = printDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    printDocument1.Print();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //print the current page.  Only good for the current page. If listview 
+            //contains more records than shown on the page, you dont see those records.
+            //So method is just good for doing a screen capture of the control as it looks
+            //at time of print.
+            Bitmap bitmap = new Bitmap(listView1.Width, listView1.Height);
+            listView1.DrawToBitmap(bitmap, listView1.ClientRectangle);
+            e.Graphics.DrawImage(bitmap, new Point(175, 150));
+        }
+    }
+
+}
